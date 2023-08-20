0fb1f5e8d8ea543114c5708ba18f56dda9dc29b1
Maxim Byts <maxim.90svet@gmail.com>
2023-08-20 21:25:13 +0300

    add 1.11 solution

---
+++ sicp/chapter1/11.rkt
@@ -0,0 +1,38 @@
+#lang sicp
+
+(#%require rackunit)
+
+#|
+  f(n) = n | n < 3
+  f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) | n >= 3
+|#
+
+(define (f n)
+  (if (< n 3)
+      n
+      (+ (f (- n 1))
+         (* (f (- n 2)) 2)
+         (* (f (- n 3)) 3))))
+
+(define (f-iter n)
+  (define (f count n1 n2 n3)
+    (cond ((< count 3) count)
+          ((= count 3) n1)
+          (else (f (- count 1)
+                   (+ n1 (* n2 2) (* n3 3))
+                   n1
+                   n2))))
+  (f n 4 2 1))
+
+(check-equal? (f-iter 1) 1)
+(check-equal? (f-iter 2) 2)
+(check-equal? (f-iter 3) 4)
+(check-equal? (f-iter 4) 11)
+(check-equal? (f-iter 5) 25)
+(check-equal? (f-iter 6) 59)
+(check-equal? (f 1) 1)
+(check-equal? (f 2) 2)
+(check-equal? (f 3) 4)
+(check-equal? (f 4) 11)
+(check-equal? (f 5) 25)
+(check-equal? (f 6) 59)
