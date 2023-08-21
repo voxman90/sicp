a3d85f5a814ec9b3f8192fd88a6cc7bca903075e
Maxim Byts <maxim.90svet@gmail.com>
2023-08-21 03:52:19 +0300

    update 1.2, 1.3, 1.4, 1.5, 1.6 solutions

--- sicp/chapter1/06.rkt
+++ sicp/chapter1/06.rkt
@@ -1,71 +1,91 @@
 #lang sicp
 
 #|
-Программа зациклится, потому что из-за того, что Scheme применяет аппликативный порядок вычисления,
-она попытается осуществить вычисление аргументов, а так как один из аргументов ссылается на самого 
-себя, то получится цепочка подстановок, которая никогда не завершится:
-(new-if (good? guess x) guess (sqrt (improve guess x) x))) =>
-(new-if ... (new-if (good? (improve guess x) x) (sqrt (improve (improve guess x) x) x))))
-В случае встроенного оператора ветвления if - предикат вычисляется предварительно, а после вычисляется
-либо одна, либо другая ветка.
+  Упражнение 1.6
+
+  Лиза П. Хакер не понимает, почему if должна быть особой формой. «Почему нельзя просто
+  определить ее как обычную процедуру с помощью cond?» — спрашивает она. Лизина подруга
+  Ева Лу Атор утверждает, что, разумеется, можно, и определяет новую версию if:
+
+    (define (new-if predicate then-clause else-clause)
+      (cond (predicate then-clause)
+            (else else-clause)))
+
+  Ева показывает Лизе новую программу:
+
+    (new-if (= 2 3) 0 5) => 5
+
+    (new-if (= 1 1) 0 5) => 0
+
+  Обрадованная Лиза переписывает через new-if программу вычисления квадратного корня:
+
+    (define (sqrt-iter guess x)
+      (new-if (good-enough? guess x)
+              guess
+              (sqrt-iter (improve guess x)
+                         x)))
+
+  Что получится, когда Лиза попытается использовать эту процедуру для вычисления квадратных корней?
+  Объясните.
 |#
 
-
-(define (never x)
-  (if #t
-      x
-      (/ x 0)))
-
-(never 1)
-
-
 #|
-  Это не выдаст ошибку 'division by zero'.
+  Процедура sqrt-iter зациклится, потому что интерпретатор использует аппликативный порядок
+  вычисления. Оператор ветвления if в Scheme устроен таким образом, что откладывает вычисление
+  выражений в ветках непосредственно до перехода к ним. Процедура new-if таким достоинством не
+  отбладает, а так как содержимое веток передаётся через параметры than-clause и else-clause,
+  то интерпретатор осуществит подстановку аргументов и начнёт их вычислять. Так как один из
+  аргументов ссылается на new-if (через sqrt-iter), то процедура зациклится.
+
+  Проиллюстрируем это на примере вызова функции sqrt-iter(1, 2):
+
+    (sqrt-iter guess x) => (sqrt-iter 1 2) =>
+    => (new-if (good-enough? 1 2) 1 (sqrt-iter (improve 1 2) 2)) => ...
+    => (new-if #f 1 (sqrt-iter (improve 1 2) 2)) => ... => (new-if #f 1 (sqrt-iter 1.5 2)) =>
+    => (new-if #f 1 (new-if (good-enough? 1.5 2) 1.5 (sqrt-iter (improve 1.5 2) 2)) => ...
+    => (new-if #f 1 (new-if #f 1.5 (new-if #f ... (new-if #t 1.995 (new-if #t 1.9975 ... => ...
+
+  Даже когда предикат good-enough? возвращает #t, интерпретатор продолжает подстановку и вычисление
+  выражений переданных в качестве аргументов и не попадёт в тело процедуры new-if.
+
+  Легко убедится в этом можно, добавив вывод в консоль в процедурах good-enough?, improve и new-if.
+  В консоли мы увидим что-то вроде:
+
+    >"good-enough?"improve"good-enough?"improve"good-enough?"improve"good-enough?"improve"...
 |#
 
 #|
-(define (custom-if predicate then-clause else-clause)
+(define (new-if predicate then-clause else-clause)
+  (newline)
+  (write "new-if")
+  (newline)
   (cond (predicate then-clause)
         (else else-clause)))
 
+(define (square x)
+  (* x x))
 
-(define (ever x)
-  (custom-if #t
-             x
-             (/ x 0)))
+(define (average x y)
+  (/ (+ x y) 2))
 
-(ever 1)
-|#
+(define (abs x)
+  (if (< x 0)
+      (- x)
+      x))
 
-#|
-  Это выдаёт ошибку 'division by zero'.
-|#
+(define (improve guess x)
+  (display "improve")
+  (average guess (/ x guess)))
 
-#| 
-  Проверим на факториале.
-|#
+(define (good-enough? guess x)
+  (write "good-enough?")
+  (< (abs (- (square guess) x))))
 
-(define (good-fac x)
-  (if (<= x 1)
-      1
-      (* x (good-fac (- x 1)))))
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x)
+                      x)))
 
-(good-fac 5)
-
-#|
-  Работает! Как только доходим до 1, то программа заканчивает вычисление
-  и не пытается вычислять else-clause дальше.
-|#
-
-#|
-(define (bad-fac x)
-  (custom-if (<= x 1)
-      1
-      (* x (bad-fac (- x 1)))))
-
-(bad-fac 5)
-|#
-
-#|
-  Мы получили бесконечный цикл.
+(sqrt-iter 1 2)
 |#
