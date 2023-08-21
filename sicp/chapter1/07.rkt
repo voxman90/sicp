bbfe22a4b69ed17358f25780972c65349264e752
Maxim Byts <maxim.90svet@gmail.com>
2023-08-21 05:24:10 +0300

    update 1.7, 1.8 solutions

--- sicp/chapter1/07.rkt
+++ sicp/chapter1/07.rkt
@@ -1,7 +1,20 @@
 #lang sicp
 
-(define (square x)
-  (* x x))
+#|
+  Упражнение 1.7
+
+  Проверка good-enough?, которую мы использовали для вычисления квадратных корней, будет
+  довольно неэффективна для поиска квадратных корней от очень маленьких чисел. Кроме того,
+  в настоящих компьютерах арифметические операции почти всегда вычисляются с ограниченной
+  точностью. Поэтому наш тест оказывается неадекватным и для очень больших чисел.
+  Альтернативный подход к реализации good-enough? состоит в том, чтобы следить, как от одной
+  итерации к другой изменяется guess, и остановиться, когда изменение оказывается небольшой
+  долей значения приближения. Разработайте процедуру вычисления квадратного корня square-root,
+  которая использует такой вариант проверки на завершение. Верно ли, что на больших и маленьких
+  числах она работает лучше?
+|#
+
+(#%require rackunit)
 
 (define (average x y)
   (/ (+ x y) 2))
@@ -20,30 +33,55 @@
 
 (define (good-enough? guess improved-guess)
   (< (abs (- improved-guess guess))
-     (get-part guess 0.01)))
+     (get-part guess 0.001)))
 
 (define (sqrt-iter guess improved-guess x)
   (if (good-enough? guess improved-guess)
       improved-guess
       (sqrt-iter improved-guess (improve improved-guess x) x)))
 
-(define (sqrt x)
+(define (square-root-alt x)
   (sqrt-iter 1 (improve 1 x) x))
 
-(define (good-enough-alt? guess x)
-  (< (abs (- (square guess) x))
-     0.01))
+(check-equal? (round (* 1000 (square-root-alt 4.0))) 2000.0)
+(check-equal? (round (* 1000 (square-root-alt 100.0))) 10000.0)
+(check-equal? (round (* 1000 (square-root-alt 1000000.0))) 1000000.0)
+(check-equal? (round (* 1000 (square-root-alt 0.04))) 200.0)
+(check-equal? (round (* 1000 (square-root-alt 999999999991111.0))) 31622776602.0)
+(check-equal? (round (* 1000 (square-root-alt 10000000000000000.0))) 100000000000.0)
 
-(define (sqrt-iter-alt guess x)
-  (if (good-enough-alt? guess x)
-      guess
-      (sqrt-iter-alt (improve guess x) x)))
+#|
+  Версия sqrt-iter с менее продвинутой проверкой приближения:
+|#
 
-(define (sqrt-alt x)
-  (sqrt-iter-alt 1 x))
+(define (square x)
+  (* x x))
 
-(define (get-difference-with-approximation x approx-func)
-  (abs (- (square (approx-func x)) x)))
+(define (square-root x)
+  (define (good-enough? guess x)
+    (< (abs (- (square guess) x))
+       0.0001))
+
+  (define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x) x)))
+
+  (sqrt-iter 1 x))
+
+(check-equal? (round (* 1000 (square-root 4.0))) 2000.0)
+(check-equal? (round (* 1000 (square-root 100.0))) 10000.0)
+(check-equal? (round (* 1000 (square-root 1000000.0))) 1000000.0)
+(check-equal? (round (* 1000 (square-root 0.04))) 200.0)
+(check-equal? (round (* 1000 (square-root 10000000000000000.0))) 100000000000.0)
+
+#|
+(check-equal? (round (* 1000 (square-root 999999999991111.0))) 31622776602.0)
+|#
+
+#|
+  Сравнение и вывод результатов работы двух процедур:
+|#
 
 (define (sign x)
   (if (< x 0)
@@ -52,46 +90,54 @@
           1
           0)))
 
-(define (who-is-closer x approx-func-1 approx-func-2)
-  (define diff-1 (get-difference-with-approximation x approx-func-1))
-  (define diff-2 (get-difference-with-approximation x approx-func-2))
-  (case (sign (- diff-2 diff-1))
-        (1 approx-func-1)
-        (-1 approx-func-2)
-        (0 "equal")))
+(define (abs-diff x y)
+  (abs (- (square y) x)))
 
 (define (displayln str)
   (display str)
   (newline))
 
-(define (display-result open-str close-str var result)
+(define (display-result open-str var close-str result)
   (display open-str)
   (display var)
   (display close-str)
   (displayln result))
 
-(define (display-better-sqrt-approx x)
-  (define sqrt-result (sqrt x))
-  (define sqrt-alt-result (sqrt-alt x))
-  (display-result "(sqrt " ") => " x (exact->inexact sqrt-result))
-  (display-result "(sqrt-alt " ") => " x (exact->inexact sqrt-alt-result))
-  (display-result "(who-is-closer " ") => " x (who-is-closer x sqrt-alt sqrt)))
+(define (who-is-closer x)
+  (define diff-alt (abs-diff x (square-root-alt x)))
+  (define diff (abs-diff x (square-root x)))
+  (case (sign (- diff-alt diff))
+        [-1 "square-root-alt"]
+        [1 "square-root"]
+        [else "equal"]))
 
-(display-better-sqrt-approx 1)
-(display-better-sqrt-approx 16)
-(display-better-sqrt-approx 100)
-(display-better-sqrt-approx 10000)
-(display-better-sqrt-approx 0.01)
+(define (display-better-sqrt-approx x)
+  (define result-alt (square-root-alt x))
+  (define result (square-root x))
+  (display-result "(square-root-alt " x ") => " result-alt)
+  (display-result "(square-root " x ") => " result)
+  (display-result "(who-is-closer " x ") => " (who-is-closer x)))
+
+(display-better-sqrt-approx 1.0)
+(display-better-sqrt-approx 4.0)
+(display-better-sqrt-approx 16.0)
+(display-better-sqrt-approx 100.0)
+(display-better-sqrt-approx 1000000.0)
+(display-better-sqrt-approx 999999999999999.0)
+(display-better-sqrt-approx 0.04)
 (display-better-sqrt-approx 0.0001)
+(display-better-sqrt-approx 0.00000001)
 
 #|
-  Функция sqrt-alt полноценно не работает для достаточно маленьких чисел (меньших погрешности).
-  Функция sqrt учитывет этот недостаток и хорошо справляется с маленькими числами.
-  Тесты показывают равный результат для больших чисел и я пока не понимаю, связано ли это с
-  особенностями представления чисел в языке или чем-то ещё. Я вернусь к этой части вопроса, когда
-  узнаю об этом больше.
-  По логике вещей, погрешность для достаточно больших чисел у функции sqrt-alt должна давать более
-  приближенный результат, вопрос только в том, насколько точны вычесления в lisp для таких чисел.
-  В JS наблюдается такое поведение, что число n выше MAX_SAFE_INTEGER, может быть n === n + 1.
-  Тут может быть такая же проблема.
+  Верно, что процедура square-root-alt работает для малых чисел лучше, чем процедура square-root,
+  т.к. её критерий проверки приближения изменяется в зависимости от величины приближения, составляя
+  от последнего постоянную долю (например, одну тысячную). Критерий проверки square-root статичен
+  (например, одна тысячная) и если приближаемое число слишком мало, процедура становится неточной.
+
+  Проверки показывают, что для больших чисел обе процедуры дают хороший результат. Для достаточно
+  больших чисел, процедура square-root начинает давать более точные результаты чем square-root-alt.
+  Но есть нюанс. Иногда она зацикливается, т.к. приближенное значение из-за неточности вычислений не
+  может удовлетворить критерий проверки. Например, для числа 999999999991111.0 square-root вообще не
+  выдаст результат (а square-root-alt выдаст).
+  Так что процедура square-root-alt лучше и для больших чисел.
 |#
--- sicp/chapter1/08.rkt
+++ sicp/chapter1/08.rkt
@@ -1,5 +1,20 @@
 #lang sicp
 
+#|
+  Упражнение 1.8
+
+  Метод Ньютона для кубических корней основан на том, что если y является приближением
+  к кубическому корню из x, то мы можем получить лучшее приближение по формуле:
+
+      x/y^2 + 2y
+     ------------
+          3
+
+  С помощью этой формулы напишите процедуру вычисления кубического корня cube-root, подобную
+  процедуре для квадратного корня. (В разделе 1.3.4 мы увидим, что можно реализовать общий
+  метод Ньютона как абстракцию этих процедур для квадратного и кубического корня.)
+|#
+
 (#%require rackunit)
 
 (define (square x)
@@ -21,16 +36,17 @@
 
 (define (good-enough? guess improved-guess)
   (< (abs (- improved-guess guess))
-     (get-part guess 1)))
+     (get-part guess 0.001)))
 
 (define (cube-iter guess improved-guess x)
   (if (good-enough? guess improved-guess)
       improved-guess
       (cube-iter improved-guess (improve improved-guess x) x)))
 
-(define (solution x)
-  (exact->inexact (cube-iter 1 (improve 1 x) x)))
+(define (cube-root x)
+  (cube-iter 1 (improve 1 x) x))
 
-(check-equal? (< (- (solution 8) 2) 0.001) #t)
-(check-equal? (< (- (solution 27) 3) 0.001) #t)
-(check-equal? (< (- (solution 216) 6) 0.001) #t)
+(check-equal? (round (* 1000 (cube-root 8.0))) 2000.0)
+(check-equal? (round (* 1000 (cube-root 1000.0))) 10000.0)
+(check-equal? (round (* 1000 (cube-root 1000000000.0))) 1000000.0)
+(check-equal? (round (* 1000 (cube-root 0.008))) 200.0)
