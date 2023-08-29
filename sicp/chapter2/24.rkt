c626befaa31d04c67912e2327e95e3fe0c8a134e
Maxim Byts <maxim.90svet@gmail.com>
2023-08-29 11:01:04 +0300

    add 2.4 solution

---
+++ sicp/chapter2/24.rkt
@@ -0,0 +1,47 @@
+#lang sicp
+
+#|
+  Упражнение 2.3
+
+  Вот еще одно процедурное представление для пар.
+  Проверьте для этого представления, что (car(cons x y)) возвращает x при любых двух объектах x и y.
+
+    (define (cons x y)
+      (lambda (m) (m x y)))
+
+    (define (car z)
+      (z (lambda (p q) p)))
+
+  Каково соответствующее определение cdr? (Подсказка: Чтобы проверить, что это работает, используйте
+  подстановочную модель из раздела 1.1.5.)
+|#
+
+#|
+  (define pair (cons x y))
+  (define pair (lambda (m) (m x y)))
+
+  (car pair)
+  (car (lambda (m) (m x y))))
+  ((lambda (m) (m x y))) (lambda (p q) p))
+  ((lambda ((lambda (p q) p)))))
+  ((lambda (p q) p) x y)
+  (lambda (x y) p)
+  x
+|#
+
+(#%require rackunit)
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car pair)
+  (pair (lambda (p _) p)))
+
+(define (cdr pair)
+  (pair (lambda (_ q) q)))
+
+(define example (cons 1 2))
+
+(check-equal? (car example) 1)
+(check-equal? (cdr example) 2)
+(check-equal? (cdr (cons 5 15)) 15)
