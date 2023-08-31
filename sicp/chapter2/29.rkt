7b7bf1f58efb1902c16d6530ef5659f16e413a83
Maxim Byts <maxim.90svet@gmail.com>
2023-08-31 06:42:45 +0300

    update 2.9 solution

--- sicp/chapter2/29.rkt
+++ sicp/chapter2/29.rkt
@@ -12,32 +12,49 @@
 |#
 
 #|
-  Сумма или разность интервалов (a1, a2), (b1, b2) высчитываются по правилу:
+  Сумма или разность интервалов (a₁, a₂), (b₁, b₂) высчитываются по правилу:
 
-    (a1, a2) + (b1, b2) = (a1 + b1, a2 + b2), (a1, a2) - (b1, b2) = (a1 - b2, a2 - b1)
+    (a₁, a₂) + (b₁, b₂) = (a₁ + b₁, a₂ + b₂), (a₁, a₂) - (b₁, b₂) = (a₁ - b₂, a₂ - b₁)
 
     w(a, b) = (b - a)/2,
 
-    w(a1, a2) + w(b1, b2) = (a2 - a1)/2 + (b2 - b1)/2 = (a2 - a1 + b2 - b1)/2 =
+    w(a₁, a₂) + w(b₁, b₂) = (a₂ - a₁)/2 + (b₂ - b₁)/2 = (a₂ - a₁ + b₂ - b₁)/2 =
 
-    = ((a2 + b2) - (a1 + b1))/2 = w(a1 + b1, a2 + b2)
+    = ((a₂ + b₂) - (a₁ + b₁))/2 = w(a₁ + b₁, a₂ + b₂)
 
-    w(a1 - b2, a2 - b1) = ((a2 - b1) - (a1 - b2))/2 = (a2 - b1 - a1 + b2)/2 =
+    w(a₁ - b₂, a₂ - b₁) = ((a₂ - b₁) - (a₁ - b₂))/2 = (a₂ - b₁ - a₁ + b₂)/2 =
 
-    = (a2 - a1)/2 + (b2 - b1)/2 = w(a1, a2) + w(b1, b2)
+    = (a₂ - a₁)/2 + (b₂ - b₁)/2 = w(a₁, a₂) + w(b₁, b₂)
 
   Получается, что радиус разности и суммы интервалов равен сумме радиусов интервалов.
 
-  Приведём пример, который показывает, что произведение радиусов интервалов не обязательно равно
-  радиусу произведения интервалов:
+  Приведём пример, который показывает, что произведение (отношение) радиусов интервалов может быть 
+  не равно радиусу произведения (отношения) интервалов:
 
-    i = (-4, -2), j = (2, 4), i * j = (-16, -4)
+    i = (-2, 6), j = (2, 4), i * j = (-8, 24)
 
-    w(i) = 1, w(j) = 1, w(i * j) = 6
+    w(i) = 4, w(j) = 1, w(i * j) = 16, w(i)w(j) != w(i * j)
 
-    i / j = (-4, -2) * (1/4, 1/2) = (-2, -1/2)
+    i/j = (-2, 6) * (1/4, 1/2) = (-1/2, 3)
 
-    w(i) = 1, w(j) = 1, w(i / j) = (-1/2 + 2)/2 = 3/4
+    w(i) = 4, w(j) = 1, w(i/j) = (3 + 1/2)/2 = 7/4, w(i)/w(j) != w(i/j)
+
+  Покажем для группы частных случаев, что радиус произведения интервалов зависит не только от
+  радиуса интервалов:
+
+    i₁ = (c₁ - w₁, c₁ + w₁), i₂ = (c₂ - w₂, c₂ + w₂), где cᵢ - центр, а wᵢ - радиус.
+
+    w(i₁)w(i₂) = ((c₁ + w₁) - (c₁ - w₁))/2 * ((c₂ + w₂) - (c₂ - w₂))/2 = w₁w₂
+
+  Возьмём такие положительные c₁, c₂, w₁, w₂, что c₁ - w₁ < 0 и c₂ - w₂ > 0, тогда:
+
+    w(i₁i₂) = w((c₁ - w₁)(c₂ + w₂), (c₁ + w₁)(c₂ + w₂)) =
+
+    = w(c₁c₂ - w₁c₂ + c₁w₂ - w₁w₂, c₁c₂ + w₁c₂ + c₁w₂ + w₁w₂) =
+
+    = ((c₁c₂ + w₁c₂ + c₁w₂ + w₁w₂) - (c₁c₂ - w₁c₂ + c₁w₂ - w₁w₂))/2 =
+
+    = (2w₁c₂ + 2w₁w₂)/2 = w₁c₂ + w₁w₂ = c₂w(i₁) + w(i₁)w(i₂) ∎
 |#
 
 (#%require rackunit)
@@ -76,30 +93,36 @@
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
 
-(define l -4)
-(define u -2)
+(define ac 2)
+(define aw 4)
+(define al (- ac aw))
+(define au (+ ac aw))
 
-(define l2 2)
-(define u2 4)
+(define bc 3)
+(define bw 1)
+(define bl (- bc bw))
+(define bu (+ bc bw))
 
-(define interval (make-interval l u))
-(define interval2 (make-interval l2 u2))
+(define a (make-interval al au))
+(define b (make-interval bl bu))
 
-(define interval-add (add-interval interval interval2))
-(define interval-sub (sub-interval interval interval2))
-(define interval-mul (mul-interval interval interval2))
-(define interval-div (div-interval interval interval2))
+(define width-a (width a))
+(define width-b (width b))
 
-(check-equal? (width interval) (/ (abs(- l u)) 2))
+(define interval-add (add-interval a b))
+(define interval-sub (sub-interval a b))
+(define interval-mul (mul-interval a b))
+(define interval-div (div-interval a b))
 
-(check-equal? (width interval-add) (+ (width interval)
-                                      (width interval2)))
+(check-equal? width-a (/ (abs(- al au)) 2))
+(check-equal? width-b (/ (abs(- bl bu)) 2))
 
-(check-equal? (width interval-sub) (+ (width interval)
-                                      (width interval2)))
+(check-equal? (width interval-add) (+ width-a width-b))
+(check-equal? (width interval-sub) (+ width-a width-b))
 
-(check-not-equal? (width interval-mul) (* (width interval)
-                                          (width interval2)))
+; w(i₁i₂) = c₂w(i₁) + w(i₁)w(i₂)
+(check-equal? (width interval-mul) (+ (* bc width-a)
+                                      (* width-a width-b)))
 
-(check-not-equal? (width interval-div) (/ (width interval)
-                                          (width interval2)))
+(check-not-equal? (width interval-mul) (* width-a width-b))
+(check-not-equal? (width interval-div) (/ width-a width-b))
