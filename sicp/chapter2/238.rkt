1ea1d336777280c006779ecdcbad7c95b802c1cc
Maxim Byts <maxim.90svet@gmail.com>
2023-09-04 23:18:15 +0300

    fix 2.38

--- sicp/chapter2/238.rkt
+++ sicp/chapter2/238.rkt
@@ -44,17 +44,16 @@
   (iter initial sequence))
 
 (define (fold-right op initial sequence)
-  (define (iter result rest)
+  (define (rec rest)
     (if (null? rest)
-        result
-        (iter (op (car rest) result)
-              (cdr rest))))
+        initial
+        (op (car rest) (rec (cdr rest)))))
 
-  (iter initial sequence))
+  (rec sequence))
 
 (check-equal? (fold-right / 1 (list 1 2 3)) 3/2)
 (check-equal? (fold-left / 1 (list 1 2 3)) 1/6)
-(check-equal? (fold-right list nil (list 1 2 3)) '(3 (2 (1 ()))))
+(check-equal? (fold-right list nil (list 1 2 3)) '(1 (2 (3 ()))))
 (check-equal? (fold-left list nil (list 1 2 3)) '(((() 1) 2) 3))
 
 #|
