be36531b6e3c775b7754473df7b3ef41dae5acbe
Maxim Byts <maxim.90svet@gmail.com>
2023-09-14 02:41:55 +0300

    add 2.80

---
+++ sicp/chapter2/280.rkt
@@ -0,0 +1,312 @@
+#lang racket
+
+#|
+  Упражнение 2.80
+
+  Определите обобщенный предикат =zero?, который проверяет, равен ли его аргумент нулю, и вставьте его
+  в пакет обобщенной арифметики. Предикат должен работать для обычных, рациональных и комплексных чисел.
+|#
+
+(#%require rackunit
+           compatibility/mlist)
+
+(define (make-table)
+  (let ((local-table (mlist '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (massoc key-1 (mcdr local-table))))
+        (if subtable
+          (let ((record (massoc key-2 (mcdr subtable))))
+            (if record
+                (mcdr record)
+                #f))
+          #f)))
+
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (massoc key-1 (mcdr local-table))))
+        (if subtable
+          (let ((record (massoc key-2 (mcdr subtable))))
+            (if record
+                (set-mcdr! record value)
+                (set-mcdr! subtable
+                           (mcons (mcons key-2 value)
+                                  (mcdr subtable)))))
+          (set-mcdr! local-table
+                     (mcons (mlist key-1
+                                   (mcons key-2 value))
+                            (mcdr local-table)))))
+      'ok)
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
+
+(define (square x) (* x x))
+
+(define (attach-tag type-tag contents)
+  (cond ((eq? type-tag 'scheme-number) contents)
+        (else (cons type-tag contents))))
+
+(define (type-tag datum)
+  (cond ((pair? datum) (car datum))
+        ((number? datum) 'scheme-number)
+        (else
+         (error "Incorrect tagged data -- TYPE-TAG" datum))))
+
+(define (contents datum)
+  (cond ((pair? datum) (cdr datum))
+        ((number? datum) datum)
+        (else
+         (error "Incorrect tagged data -- TYPE-TAG" datum))))
+
+; Пакет арифметических операций над числами
+
+(define (install-scheme-number-package)
+  (define (tag x) (attach-tag 'scheme-number x))
+
+  (put 'add '(scheme-number scheme-number)
+    (lambda (x y) (tag (+ x y))))
+  (put 'sub '(scheme-number scheme-number)
+    (lambda (x y) (tag (- x y))))
+  (put 'mul '(scheme-number scheme-number)
+    (lambda (x y) (tag (* x y))))
+  (put 'div '(scheme-number scheme-number)
+    (lambda (x y) (tag (/ x y))))
+  (put 'make 'scheme-number
+    (lambda (x) (tag x)))
+  'done)
+
+; Пакет арифметических операций над рациональными числами
+
+(define (install-rational-package)
+  (define (numer x) (car x))
+
+  (define (denom x) (cdr x))
+
+  (define (make-rat n d)
+    (let ((g (gcd n d)))
+      (cons (/ n g) (/ d g))))
+
+  (define (add-rat x y)
+    (make-rat (+ (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+
+  (define (sub-rat x y)
+    (make-rat (- (* (numer x) (denom y))
+                 (* (numer y) (denom x)))
+              (* (denom x) (denom y))))
+
+  (define (mul-rat x y)
+    (make-rat (* (numer x) (numer y))
+              (* (denom x) (denom y))))
+
+  (define (div-rat x y)
+    (make-rat (* (numer x) (denom y))
+              (* (denom x) (numer y))))
+
+  (define (tag x) (attach-tag 'rational x))
+
+  (put 'add '(rational rational)
+    (lambda (x y) (tag (add-rat x y))))
+  (put 'sub '(rational rational)
+    (lambda (x y) (tag (sub-rat x y))))
+  (put 'mul '(rational rational)
+    (lambda (x y) (tag (mul-rat x y))))
+  (put 'div '(rational rational)
+    (lambda (x y) (tag (div-rat x y))))
+  (put 'make 'rational
+    (lambda (n d) (tag (make-rat n d))))
+  'done)
+
+; Пакеты арифметических операций над комплексными числами в полярном, декартовом и обобщённом
+; представлении
+
+(define (install-polar-package)
+  (define (magnitude z) (car z))
+
+  (define (angle z) (cdr z))
+
+  (define (make-from-mag-ang r a) (cons r a))
+
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+
+  (define (tag x) (attach-tag 'polar x))
+
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+    (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-rectangular-package)
+  (define (real-part z) (car z))
+
+  (define (imag-part z) (cdr z))
+
+  (define (make-from-real-imag x y) (cons x y))
+
+  (define (magnitude z)
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))))
+
+  (define (angle z)
+    (atan (imag-part z) (real-part z)))
+
+  (define (make-from-mag-ang r a)
+    (cons (* r (cos a)) (* r (sin a))))
+
+  (define (tag x) (attach-tag 'rectangular x))
+
+  (put 'real-part '(rectangular) real-part)
+  (put 'imag-part '(rectangular) imag-part)
+  (put 'magnitude '(rectangular) magnitude)
+  (put 'angle '(rectangular) angle)
+  (put 'make-from-real-imag 'rectangular
+    (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'rectangular
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+(define (install-complex-package)
+  (define (real-part z)
+    (apply-generic 'real-part z))
+
+  (define (imag-part z)
+    (apply-generic 'imag-part z))
+
+  (define (magnitude z)
+    (apply-generic 'magnitude z))
+
+  (define (angle z)
+    (apply-generic 'angle z))
+
+  (define (make-from-real-imag x y)
+    ((get 'make-from-real-imag 'rectangular) x y))
+
+  (define (make-from-mag-ang r a)
+    ((get 'make-from-mag-ang 'polar) r a))
+
+  (define (add-complex z1 z2)
+    (make-from-real-imag (+ (real-part z1) (real-part z2))
+                         (+ (imag-part z1) (imag-part z2))))
+
+  (define (sub-complex z1 z2)
+    (make-from-real-imag (- (real-part z1) (real-part z2))
+                         (- (imag-part z1) (imag-part z2))))
+
+  (define (mul-complex z1 z2)
+    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+                       (+ (angle z1) (angle z2))))
+
+  (define (div-complex z1 z2)
+    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+                       (- (angle z1) (angle z2))))
+
+  (define (tag z) (attach-tag 'complex z))
+
+  (put 'add '(complex complex)
+    (lambda (z1 z2) (tag (add-complex z1 z2))))
+  (put 'sub '(complex complex)
+    (lambda (z1 z2) (tag (sub-complex z1 z2))))
+  (put 'mul '(complex complex)
+    (lambda (z1 z2) (tag (mul-complex z1 z2))))
+  (put 'div '(complex complex)
+    (lambda (z1 z2) (tag (div-complex z1 z2))))
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle)
+  (put 'make-from-real-imag 'complex
+    (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'complex
+    (lambda (r a) (tag (make-from-mag-ang r a))))
+  'done)
+
+; Обобщённые процедуры
+
+(define (apply-generic op . args)
+  (let ((type-tags (map type-tag args)))
+    (let ((proc (get op type-tags)))
+      (if proc
+          (apply proc (map contents args))
+          (error
+            "There is no method for this type -- APPLY-GENERIC"
+            (list op type-tags))))))
+
+(define (make-rational n d)
+  ((get 'make 'rational) n d))
+
+(define (make-scheme-number num)
+  ((get 'make 'scheme-number) num))
+
+(define (make-complex-from-real-imag r i)
+  ((get 'make-from-real-imag 'complex) r i))
+
+(install-scheme-number-package)
+(install-rational-package)
+(install-rectangular-package)
+(install-polar-package)
+(install-complex-package)
+
+; Решение упражения
+
+(define (update-scheme-number-package)
+  (put 'zero? '(scheme-number)
+    (lambda (x) (= x 0))))
+
+(define (update-rational-package)
+  (define (numer x) (car x))
+  (define (denom x) (cdr x))
+
+  (define (zero? x)
+    (and      (= (numer x) 0)
+         (not (= (denom x) 0))))
+
+  (put 'zero? '(rational) zero?))
+
+(define (update-complex-package)
+  (define (real-part z)
+    (apply-generic 'real-part z))
+  (define (imag-part z)
+    (apply-generic 'imag-part z))
+
+  (define (zero? x)
+    (and (= (real-part x) 0)
+         (= (imag-part x) 0)))
+
+  (put 'zero? '(complex) zero?))
+
+(define (=zero? x)
+  (apply-generic 'zero? x))
+
+; Тесты
+
+(update-scheme-number-package)
+(update-rational-package)
+(update-complex-package)
+
+(check-true (=zero? (make-complex-from-real-imag 0 0)))
+(check-true (=zero? (make-rational 0 1)))
+(check-true (=zero? (make-scheme-number 0)))
+(check-false (=zero? (make-scheme-number 5)))
+(check-false (=zero? (make-rational 1 2)))
+(check-false (=zero? (make-complex-from-real-imag 1 2)))
