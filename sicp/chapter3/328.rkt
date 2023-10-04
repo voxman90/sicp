90194552e3e19387076559a8a0adcd223e143bd7
Maxim Byts <maxim.90svet@gmail.com>
2023-10-04 08:23:12 +0300

    fix 3.28

--- sicp/chapter3/328.rkt
+++ sicp/chapter3/328.rkt
@@ -170,23 +170,21 @@
 
 ; Решение упражнения
 
-(define and-gate-delay 5)
-
 (define (logical-or s1 s2)
   (cond ((or (= s1 1) (= s2 1)) 1)
         ((and (= s1 0) (= s2 0)) 0)
         (else (error "Wrong signal -- LOGICAL-OR" s1 s2))))
 
-(define (or-gate a1 a2 output)
-  (define (and-action-procedure)
-    (let ((new-value (logical-or (get-signal a1)
-                                 (get-signal a2))))
-      (after-delay and-gate-delay
+(define (or-gate s1 s2 output)
+  (define (or-action-procedure)
+    (let ((new-value (logical-or (get-signal s1)
+                                 (get-signal s2))))
+      (after-delay or-gate-delay
         (lambda ()
           (set-signal! output new-value)))))
 
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
+  (add-action! s1 or-action-procedure)
+  (add-action! s2 or-action-procedure)
   'ok)
 
 (define a1 (make-wire))
